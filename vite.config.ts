1d9cb4048f08e7ef3818d8298e39ecaaab1f4668
Ilya Katts <ilyak@wix.com>
2024-01-05 09:07:54 +0200

    Configure css modules

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    css: {
+        modules: { generateScopedName: '[name]__[local]_[hash:base64:5]' },
+    },
 });
