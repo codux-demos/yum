4a027f8086ee44a73afc286caacfdff080eb690e
Ilya Katts <ilyak@wix.com>
2024-01-27 16:39:46 +0200

    Display real data in cities section

--- src/components/cities-section/cities-section.tsx
+++ src/components/cities-section/cities-section.tsx
@@ -1,54 +1,28 @@
+import { LinkButton } from '../link-button/link-button';
+import { Country } from '../../types';
 import classNames from 'classnames';
 import styles from './cities-section.module.scss';
-import { LinkButton } from '../link-button/link-button';
 
 export interface CitiesSectionProps {
     className?: string;
+    country: Country;
 }
 
-export const CitiesSection = ({ className }: CitiesSectionProps) => {
-    const cities = [
-        'Austin',
-        'Baltimore',
-        'Boston',
-        'Charlotte',
-        'Columbus',
-        'Dallas',
-        'Denver',
-        'Detroit',
-        'El Paso',
-        'Fort Worth',
-        'Indianapolis',
-        'Jacksonville',
-        'Las Vegas',
-        'Louisville',
-        'Memphis',
-        'Nashville',
-        'New York',
-        'Oklahoma City',
-        'Philadelphia',
-        'Phoenix',
-        'Portland',
-        'San Antonio',
-        'San Diego',
-        'San Francisco',
-        'San Jose',
-        'Seattle',
-        'Washington',
-    ];
+export const CitiesSection = ({ className, country }: CitiesSectionProps) => {
+    const cities = country.cities;
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles.content}>
                 <div className={styles.header}>
                     <h2 className={styles.title}>
-                        Yum cities in <LinkButton>United States 🇺🇸</LinkButton>
+                        Yum cities in <LinkButton>{country.name}</LinkButton>
                     </h2>
                     <LinkButton>View all Yum cities</LinkButton>
                 </div>
                 <ul className={styles.list}>
                     {cities.map((city) => (
-                        <li key={city}>
-                            <LinkButton>{city}</LinkButton>
+                        <li key={city.id}>
+                            <LinkButton>{city.name}</LinkButton>
                         </li>
                     ))}
                 </ul>
--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -4,20 +4,23 @@ import { CitiesSection } from '../cities-section/cities-section';
 import { Footer } from '../footer/footer';
 import { MoreThanDeliverySection } from '../more-than-delivery-section/more-than-delivery-section';
 import { HeaderContainer } from '../header/header-container';
+import { PartnersSection } from '../partners-section/partners-section';
+import { countries } from '../../data/coutries';
+import { Country } from '../../types';
 import classNames from 'classnames';
 import styles from './landing-page.module.scss';
-import { PartnersSection } from '../partners-section/partners-section';
 
 export interface LandingPageProps {
     className?: string;
 }
 
 export const LandingPage = ({ className }: LandingPageProps) => {
+    const country = countries.find((country) => country.id === 'US') as Country;
     return (
         <div className={classNames(styles.root, className)}>
             <HeaderContainer userAuthenticated={false} />
             <HeroSection />
-            <CitiesSection />
+            <CitiesSection country={country} />
             <WhatIsSection />
             <MoreThanDeliverySection />
             <PartnersSection />
