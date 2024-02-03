88e61c924725efe08f95c8fe0ea1c291368923a1
Ilya Katts <ilyak@wix.com>
2024-02-03 19:59:44 +0200

    Refactor icons

--- src/assets/icons/chevron-left.tsx
+++ src/assets/icons/chevron-left.tsx
@@ -1,12 +1,9 @@
 export const ChevronLeftIcon = () => {
     return (
         <svg width="40" viewBox="0 0 40 40">
-            <path
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="m15.828 19.95 8.486 8.529-1.415 1.421L13 19.95l1.414-1.421L22.9 10l1.414 1.421-8.486 8.529Z"
-            />
+            <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                <path d="m15.828 19.95 8.486 8.529-1.415 1.421L13 19.95l1.414-1.421L22.9 10l1.414 1.421-8.486 8.529Z" />
+            </g>
         </svg>
     );
 };
--- src/assets/icons/close.tsx
+++ src/assets/icons/close.tsx
@@ -1,12 +1,9 @@
 export const CloseIcon = () => {
     return (
         <svg width="40" viewBox="0 0 40 40">
-            <path
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="m12.222 10.808-1.414 1.414L18.586 20l-7.778 7.778 1.414 1.414L20 21.414l7.778 7.778 1.414-1.414L21.414 20l7.778-7.778-1.414-1.414L20 18.586l-7.778-7.778Z"
-            />
+            <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+                <path d="m12.222 10.808-1.414 1.414L18.586 20l-7.778 7.778 1.414 1.414L20 21.414l7.778 7.778 1.414-1.414L21.414 20l7.778-7.778-1.414-1.414L20 18.586l-7.778-7.778Z" />
+            </g>
         </svg>
     );
 };
