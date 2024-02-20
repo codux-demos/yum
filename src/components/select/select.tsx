b0e291e5fb8634c644c2c9a040f7d4ff6d289c96
Ilya Katts <ilyak@wix.com>
2024-02-20 15:15:25 +0200

    [WIP] Add select component

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -5,29 +5,57 @@ import {
     SelectProvider as AriakitSelectProvider,
     SelectItem as AriakitSelectItem,
     SelectPopover as AriakitSelectPopover,
+    useSelectStore,
 } from '@ariakit/react';
 import classNames from 'classnames';
 import styles from './select.module.scss';
 
+export interface SelectElementsClassNames {
+    select?: string;
+    popover?: string;
+    selectItem?: string;
+}
+
 export interface SelectProps {
     className?: string;
+    elementsClassNames?: SelectElementsClassNames;
     label?: ReactNode;
     items?: string[];
 }
 
-export const Select = ({ className, label, items }: SelectProps) => {
+export const Select = ({ className, elementsClassNames = {}, label, items }: SelectProps) => {
+    const selectStore = useSelectStore({ defaultValue: 'Apple' });
+
     return (
         <div className={classNames(styles.root, className)}>
-            <AriakitSelectProvider defaultValue="Apple">
-                {label && <AriakitSelectLabel className={styles.label}>{label}</AriakitSelectLabel>}
-                <AriakitSelect className="button" />
-                <AriakitSelectPopover gutter={4} sameWidth className="popover">
-                    {items &&
-                        items.map((item) => (
-                            <AriakitSelectItem className="select-item" value={item} />
-                        ))}
-                </AriakitSelectPopover>
-            </AriakitSelectProvider>
+            {/* TODO: make `accessibleWhenDisabled` configurable */}
+
+            {label && (
+                <AriakitSelectLabel store={selectStore} className={styles.label}>
+                    {label}
+                </AriakitSelectLabel>
+            )}
+            {/* TODO: make `accessibleWhenDisabled` configurable */}
+            <AriakitSelect
+                store={selectStore}
+                className={classNames(styles.select, elementsClassNames.select)}
+                accessibleWhenDisabled
+            />
+            <AriakitSelectPopover
+                store={selectStore}
+                gutter={4}
+                sameWidth
+                className={classNames(styles.popover, elementsClassNames.popover)}
+            >
+                {items &&
+                    items.map((item, index) => (
+                        <AriakitSelectItem
+                            key={index}
+                            className={classNames(styles.selectItem, elementsClassNames.selectItem)}
+                            value={item}
+                        />
+                    ))}
+            </AriakitSelectPopover>
         </div>
     );
 };
