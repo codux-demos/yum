928f745e3ee641f8863c022388fce06a39a300f9
Tom Raviv <tomr@wix.com>
2024-03-12 17:49:54 +0200

    fix: propagate ref all the way to ariakit

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,20 +1,22 @@
-import { BaseButton, BaseButtonProps } from '../base-button/base-button';
 import classNames from 'classnames';
+import { forwardRef } from 'react';
+import { BaseButton, BaseButtonProps } from '../base-button/base-button';
 import styles from './button.module.scss';
 
 export interface ButtonProps extends BaseButtonProps {
     variant?: 'primary' | 'secondary' | 'unstyled';
 }
 
-export const Button = ({
-    className,
-    variant = 'secondary',
-    children = 'Label',
-    ...props
-}: ButtonProps) => {
-    return (
-        <BaseButton className={classNames(styles.root, styles[variant], className)} {...props}>
-            {children}
-        </BaseButton>
-    );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, variant = 'secondary', children = 'Label', ...props }, ref) => {
+        return (
+            <BaseButton
+                className={classNames(styles.root, styles[variant], className)}
+                {...props}
+                ref={ref}
+            >
+                {children}
+            </BaseButton>
+        );
+    }
+);
