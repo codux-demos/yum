e452354268ebaada3b4bbe84b258673c1bc59c20
Avi Vahl <avi.vahl@wix.com>
2024-03-12 17:43:55 +0200

    fix: propagate ref all the way to ariakit

--- src/components/base-button/base-button.tsx
+++ src/components/base-button/base-button.tsx
@@ -1,18 +1,21 @@
 import { Button, ButtonProps } from '@ariakit/react';
 import classNames from 'classnames';
+import { forwardRef } from 'react';
 import styles from './base-button.module.scss';
 
 export interface BaseButtonProps extends ButtonProps {}
 
-export const BaseButton = ({
-    className,
-    children,
-    accessibleWhenDisabled = true,
-    ...props
-}: BaseButtonProps) => {
-    return (
-        <Button className={classNames(styles.root, className)} accessibleWhenDisabled {...props}>
-            {children}
-        </Button>
-    );
-};
+export const BaseButton = forwardRef<HTMLButtonElement, BaseButtonProps>(
+    ({ className, children, accessibleWhenDisabled = true, ...props }, ref) => {
+        return (
+            <Button
+                className={classNames(styles.root, className)}
+                accessibleWhenDisabled
+                {...props}
+                ref={ref}
+            >
+                {children}
+            </Button>
+        );
+    }
+);
